afbb5f820afca78806adb78a79c1ecd0f1079149
Nybbit <mrilliteracy@hotmail.com>
2018-03-02 17:02:02 -0800

    Update cs-template.cs
    Update namespace name

--- 2018/02 Feb/cs-template.cs
+++ 2018/02 Feb/cs-template.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace randomcs {
+namespace MonthlyChallenge {
     class Program {
         static bool isBalanced(string str) {
             // your code
