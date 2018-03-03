c25470501b6d581d058c1472416f6703a5b1ca73
Arthur-QQ <30394349+Arthur-QQ@users.noreply.github.com>
2018-03-02 21:56:13 -0300

    Create cs-template.cs

---
+++ 2018/02 Feb/cs-template.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomcs {
+    class Program {
+        static bool isBalanced(string str) {
+            // your code
+            return true;
+        }
+        static void Main(string[] args) {
+            string[] inputs = new string[] { "[]", "[{}]", "[{]}", "[[", "]]", "}{", "[{()}]" };
+            bool[] outputs = new bool[] { true, true, false, false, false, false, true };
+            for (int i = 0; i < inputs.Length; i++) {
+                if (isBalanced(inputs[i]) != outputs[i]) {
+                    Console.WriteLine(string.Format("Wrong output on test #{0}", i+1));
+                    Console.WriteLine(string.Format("Input: {0}", inputs[i]));
+                    Console.WriteLine(string.Format("Expected output: {0}", outputs[i]));
+                    System.Environment.Exit(1);
+                }
+            }
+            Console.WriteLine("You\'ve passed all test cases");
+        }
+    }
+}
